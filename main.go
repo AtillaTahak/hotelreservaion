0aae8bc01010f37994680e45cca7ce46ecffc7b4
atillatahak <atillatahaa@gmail.com>
2023-07-16 23:07:01 +0300

    added mongo db and created file for db

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"hotelreservation/api"
+	"hotelreservation/db"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address for the server")
 	flag.Parse()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//handler initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
 	app := fiber.New()
 	apiV1 := app.Group("/api/v1")
 
-	apiV1.Get("/user", api.HandleGetUsers)
-	apiV1.Get("/user/:id", api.HandleGetUser)
+	apiV1.Get("/user", userHandler.HandleGetUsers)
+	apiV1.Get("/user/:id", userHandler.HandleGetUser)
 	app.Listen(*listenAddr)
 }
